067c8ecbc0909deee439b137cf20f8c79d80c055
Mv41-dev <larenblk@gmail.com>
2026-08-18 09:24:40 -0300

    18/06

--- player.gd
+++ player.gd
@@ -10,6 +10,8 @@ var gravity = 980.0 # Força da gravidade (Positivo puxa para BAIXO)
 # A função _physics_process roda 60 vezes por segundo (é o coração da física)
 func _physics_process(delta):
 	# 1. Aplicar Gravidade se não estiver no chão (is_on_floor verifica o TileMap)
+	
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta # Delta ajusta a queda para ser suave em qualquer PC
 
@@ -21,10 +23,13 @@ func _physics_process(delta):
 	# get_axis retorna -1 (Esquerda), 1 (Direita) ou 0 (Parado)
 	var direction = Input.get_axis("left", "right")
 	
+	
 	if direction: # Se o jogador apertar algum botão de andar
 		velocity.x = direction * SPEED 
 		anim.play("run") 
 		anim.flip_h = (direction < 0) # Vira a imagem
+	if Input.is_action_just_pressed("ataque"):
+		anim.play("attack") 
 	else: # Se soltou os botões
 		velocity.x = move_toward(velocity.x, 0, SPEED) 
 		anim.play("idle") 
